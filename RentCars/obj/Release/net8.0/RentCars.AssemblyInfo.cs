4fd36d8f2deaf9932f7a8ac5e12b73ab9b5a330e
Guilherme Cabrita <gui.celorindo@gmail.com>
2026-07-31 09:54:43 +0100

    Começo do projeto final

--- RentCars/obj/Release/net8.0/RentCars.AssemblyInfo.cs
+++ RentCars/obj/Release/net8.0/RentCars.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentCars")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3228ac5c8c1c740ad63a49307043e59607d97ae6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentCars")]
